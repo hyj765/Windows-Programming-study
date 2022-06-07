6e3ec43e5d5298642ec18fa09183cea067fbb6dd
HYJ <hyj765@naver.com>
2022-06-07 23:59:46 +0900

    Update registry_delete_ms-msdt.cpp

--- registry_delete_ms-msdt.cpp
+++ registry_delete_ms-msdt.cpp
@@ -8,24 +8,29 @@ public:
 
 	void deleteRegister(HKEY hkeyRoot) {
 		LONG Result;
-		Result = RegDeleteKey(hkeyRoot,TEXT("ms-msdt"));
+		Result = RegDeleteKey(hkeyRoot, TEXT("ms-msdt"));
 
 		if (Result == ERROR_SUCCESS) {
 			printf("ms-msdt delete");
+			return;
 		}
-		
+		else if(Result == 5){
+			printf("Access Denied");
+			return;
+		}
 
-		Result = RegOpenKeyEx(hkeyRoot, TEXT("ms-msdt"), 0, KEY_READ, &hKey);
+
+		Result = RegOpenKeyEx(hkeyRoot, TEXT("ms-msdt"), 0, KEY_READ, &hkey);
 
 		if (Result != ERROR_SUCCESS)
 		{
 			if (Result == ERROR_FILE_NOT_FOUND) {
 				printf("Key not found.\n");
-				
+
 			}
 			else {
 				printf("Error opening key.\n");
-				
+
 			}
 		}
 
